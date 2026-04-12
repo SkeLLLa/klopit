adc21f7cc98dd8f1fbbab17b7d9c2264e0a9f4c3
Commit User <code.commit.user@gmail.com>
2026-04-12 15:12:26 +0200

    feat: add github releases and changelog

--- .release/github.js
+++ .release/github.js
@@ -2,5 +2,6 @@ module.exports = [
   '@semantic-release/github',
   {
     message: 'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}',
+    assets: [{ path: 'klopit-pages.tar.gz', label: 'Pages bundle (${nextRelease.gitTag})' }],
   },
 ];
